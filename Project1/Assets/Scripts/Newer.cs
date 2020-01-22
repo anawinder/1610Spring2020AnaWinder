ab86ed115683a332ba0e3e5f4bdfe613e4b02f1b
Ana Winder <ana.filigrana@yahoo.com>
2020-01-22 11:23:24 -0700

    Events
    Created new Script Newer

---
+++ Project1/Assets/Scripts/Newer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Newer : MonoBehaviour
+{
+
+    public float floatNumber;
+    public int scoreValue;
+    public UnityEvent uEvent;
+    
+    
+    void Start()
+    {
+        uEvent.Invoke();
+    }
+
+    void Update()
+    {
+        
+    }
+}
