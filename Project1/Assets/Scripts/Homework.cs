196be545e38344ff9d13b8be887f7d73103a5c60
Ana Winder <ana.filigrana@yahoo.com>
2020-01-22 16:20:43 -0700

    Homework
    Practiced writing codes for Events

---
+++ Project1/Assets/Scripts/Homework.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Homework : MonoBehaviour
+{
+
+    public float floatNumber;
+    public int scoreValue;
+    public UnityEvent uEvent;
+    
+    
+    // Practice from Class
+    void Start()
+    {
+        uEvent.Invoke();
+    }
+
+    void Update()
+    {
+        
+    }
+}
