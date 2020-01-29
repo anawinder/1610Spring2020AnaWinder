065e1ef71cf8e6efb69f758a683c6571ed985e89
Ana Winder <ana.filigrana@yahoo.com>
2020-01-29 11:18:40 -0700

    MouseEvents
    Created 6 mouse events and added a flare

---
+++ Project1/Assets/Scripts/MouseEvents.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseEvents : MonoBehaviour
+{
+    
+    public UnityEvent mouseDownEvent;
+    
+    private void OnMouseDown()
+    {
+        mouseDownEvent.Invoke();
+    }
+
+    public UnityEvent mouseDragEvent;
+    public void OnMouseDrag()
+    {
+        mouseDragEvent.Invoke();
+    }
+
+    public UnityEvent mouseUpEvent;
+    private void OnMouseUp()
+    {
+        mouseUpEvent.Invoke();
+    }
+
+    public UnityEvent mouseEnterEvent;
+    private void OnMouseEnter()
+    {
+        mouseEnterEvent.Invoke();
+    }
+
+    public UnityEvent mouseExitEvent;
+    private void OnMouseExit()
+    {
+        mouseExitEvent.Invoke();
+    }
+
+    public UnityEvent mouseOverEvent;
+    private void OnMouseOver()
+    {
+        mouseOverEvent.Invoke();
+    }
+}
