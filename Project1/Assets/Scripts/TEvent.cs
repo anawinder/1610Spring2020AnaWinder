668755780347586d97640d009a71ffcb2331415a
Ana Winder <ana.filigrana@yahoo.com>
2020-01-29 11:18:11 -0700

    TEvents
    Homework

---
+++ Project1/Assets/Scripts/TEvent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Dynamic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TEvent : MonoBehaviour
+{
+    public int color;
+    public float number;
+    public string deku;
+    public UnityEvent anime;
+    public UnityEvent manga;
+    public UnityEvent comic;
+    private TriggerEvents data;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        anime.Invoke();
+        print(color);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        manga.Invoke();
+        print(number);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        comic.Invoke();
+        print(deku);
+    }
+}
+
