869a648262ac0c1fff32f36825bab6f9a9db4639
Ana Winder <ana.filigrana@yahoo.com>
2020-02-19 11:18:39 -0700

    Health Bar
    Created Health bar, Player, and Killer

---
+++ Project1/Assets/Scripts/ImageBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ImageBehaviour : MonoBehaviour
+{
+
+    private Image Imageobj;
+    void Start()
+    {
+        Imageobj = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
